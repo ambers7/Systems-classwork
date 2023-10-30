e3db4b2d66b1325fd28fa9f604bfd25dbbeb81de
ambers7 <ambrshen@gmail.com>
2023-10-30 18:30:03 -0400

    Fix no input a.out

--- 10-30/main.c
+++ 10-30/main.c
@@ -13,30 +13,29 @@ int printvalid(int x, int y, int z) {//1 if valid, 0 if not
   }
 }
 int main(int argc, char* argv[]) {
-    for (int i = 0;i<argc;i++) {
-      printf("i: %d %s\n",i,argv[i]);
-    }
+  if (argv[1]!=NULL) {
     if (strcmp(argv[1],"user")==0) {
       int c = 0;
       char st[100];
       while(fgets(st,100,stdin)) {
+        printf("hi");
         int x; int y; int z;
         sscanf(st,"%d %d %d",&x,&y,&z);
         if (printvalid(x,y,z)==1) c++;
       }
       printf("Number of valid triangles: %d\n", c);
+    } else if (strcmp(argv[1],"horizontal")==0) {
+      int count = 0;
+      char s[100];
+      FILE * f = fopen("./input.txt","r");
+      while(fgets(s,100,f)) {
+        int x; int y; int z;
+        sscanf(s,"%d %d %d",&x,&y,&z);
+        if (printvalid(x,y,z)==1) count++;
+      }
+      printf("Number of valid triangles: %d\n", count);
     }
-  else if (strcmp(argv[1],"horizontal")==0) {
-    int count = 0;
-    char s[100];
-    FILE * f = fopen("./input.txt","r");
-    while(fgets(s,100,f)) {
-      int x; int y; int z;
-      sscanf(s,"%d %d %d",&x,&y,&z);
-      if (printvalid(x,y,z)==1) count++;
-    }
-    printf("Number of valid triangles: %d\n", count);
   } else {
-      printf("Directions: Pass in number and string(\"user or horizontal\") after ./a.out\n");
-  }
+        printf("Directions: Pass in number and string(\"user or horizontal\") after ./a.out\n");
+    }
 }
